91bb482b5c1968e6e60d9e6074bd24a0b5205dcd
srai <srai@yahoo-inc.com>
2014-06-03 09:31:30 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Satyajit Rai on 6/3/14.
+//  Copyright (c) 2014 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
